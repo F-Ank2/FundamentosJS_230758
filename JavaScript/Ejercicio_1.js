ddf56686d0e26a036fdcb6328d0421121b559999
Francisco Garcia Garcia <franciscogarciagarcia911@gmail.com>
2024-10-10 15:24:45 -0600

    Practica 3
    Inicializacion de la practica 1 de JS declaracion de variables undefinded, boolean y number

---
+++ JavaScript/Ejercicio_1.js
@@ -0,0 +1,16 @@
+//Ejercicio 1
+
+var name = "Francisco";
+var lastname;
+var age = 20;
+
+//El objeto de console nos permite enviar datos a la terminal y poder visualizar
+// el valor de la variable o el resultado de la funcion o metodo
+
+console.warn("---Declaracion de variables del tipo constante usando: VAR");
+console.log("Intentando leer las variables",name, lastname);
+
+lastname = "Garcia";
+console.log("Una vez inicializadas las vuelvo a llamar",name, lastname);
+
+//Una variable puede cambiar en el proceso de ejecucion del programa
